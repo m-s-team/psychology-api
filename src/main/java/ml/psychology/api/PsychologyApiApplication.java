4290261e7fbac7dacd99401dd03a126db9aac4fb
Hasan Karimi <hasankarimi.dev@gmail.com>
2022-07-11 18:31:58 +0430

    initial project

---
+++ src/main/java/ml/psychology/api/PsychologyApiApplication.java
@@ -0,0 +1,13 @@
+package ml.psychology.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsychologyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsychologyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ml/psychology/api/PsychologyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ml.psychology.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsychologyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
