e994fff69c24c91e0624c31af869e2f9a8c9904d
Hasan Karimi <hasankarimi.dev@gmail.com>
2022-07-13 15:43:47 +0430

    feat: config h2 database
    initial configuration of h2 for dev
    environment. config security for h2
    console. and initial database schema.

---
+++ src/main/java/ml/psychology/api/config/SecurityConfiguration.java
@@ -0,0 +1,25 @@
+package ml.psychology.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.Profiles;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, Environment env) throws Exception {
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable();
+
+        if (env.acceptsProfiles(Profiles.of("dev")))
+            http.authorizeRequests().antMatchers("/h2-console/**").permitAll();
+
+        http.authorizeRequests().anyRequest().authenticated();
+
+        return http.build();
+    }
+}
