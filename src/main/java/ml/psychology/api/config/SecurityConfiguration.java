cf33d2221a27e477737d92aad4365b55147c47f3
Hasan Karimi <hasankarimi.dev@gmail.com>
2022-07-28 16:35:53 +0430

    feat: add openapi-ui dependency

--- src/main/java/ml/psychology/api/config/SecurityConfiguration.java
+++ src/main/java/ml/psychology/api/config/SecurityConfiguration.java
@@ -5,10 +5,22 @@ import org.springframework.core.env.Environment;
 import org.springframework.core.env.Profiles;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 public class SecurityConfiguration {
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web ->
+                web
+                        .ignoring()
+                        .antMatchers("/swagger-ui/**")
+                        .antMatchers("/v3/api-docs/**")
+                        .antMatchers("/actuator/**");
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http, Environment env) throws Exception {
         http
