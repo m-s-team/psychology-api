fb2d72e30e925432b166c68f37cab093ba0fcb12
Hasan Karimi <hasankarimi.dev@gmail.com>
2022-07-30 03:17:40 +0430

    feat: config the oauth2 security of the resource server

--- src/main/java/ml/psychology/api/config/SecurityConfiguration.java
+++ src/main/java/ml/psychology/api/config/SecurityConfiguration.java
@@ -1,15 +1,25 @@
 package ml.psychology.api.config;
 
+import ml.psychology.api.security.oauth2.AudienceValidator;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 import org.springframework.core.env.Profiles;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.*;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 public class SecurityConfiguration {
+
+    @Value("${auth0.audience}")
+    private String audience;
+    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+    private String issuerUri;
 
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
@@ -25,13 +35,29 @@ public class SecurityConfiguration {
     public SecurityFilterChain filterChain(HttpSecurity http, Environment env) throws Exception {
         http
                 .csrf().disable()
+                .cors().and()
                 .headers().frameOptions().disable();
 
         if (env.acceptsProfiles(Profiles.of("dev")))
             http.authorizeRequests().antMatchers("/h2-console/**").permitAll();
 
-        http.authorizeRequests().anyRequest().authenticated();
+        http
+                .authorizeRequests().anyRequest().authenticated().and()
+                .oauth2ResourceServer().jwt();
 
         return http.build();
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder() {
+        NimbusJwtDecoder jwtDecoder = JwtDecoders.fromOidcIssuerLocation(issuerUri);
+
+        OAuth2TokenValidator<Jwt> audienceValidator = new AudienceValidator(audience);
+        OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(issuerUri);
+        OAuth2TokenValidator<Jwt> withAudience = new DelegatingOAuth2TokenValidator<>(withIssuer, audienceValidator);
+
+        jwtDecoder.setJwtValidator(withAudience);
+
+        return jwtDecoder;
     }
 }
