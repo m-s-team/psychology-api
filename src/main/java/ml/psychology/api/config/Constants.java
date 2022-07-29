b977cced8a47050efde5776d461dafe18eb9f9d5
Hasan Karimi <hasankarimi.dev@gmail.com>
2022-07-30 03:24:26 +0430

    feat: add user domain

---
+++ src/main/java/ml/psychology/api/config/Constants.java
@@ -0,0 +1,6 @@
+package ml.psychology.api.config;
+
+public final class Constants {
+    // Regex for acceptable username
+    public static final String LOGIN_REGEX = "^(?>[a-zA-Z0-9!$&*+=?^_`{|}~.-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*)|(?>[_.@A-Za-z0-9-]+)$";
+}
---
+++ src/main/java/ml/psychology/api/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package ml.psychology.api.repository;
+
+import ml.psychology.api.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+}
