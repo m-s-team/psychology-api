bfd0e45605a4dd713d6a31f845ea99c8970f9e4a
Hasan Karimi <hasankarimi.dev@gmail.com>
2022-08-29 14:55:15 +0430

    feat: add sequential reasoning subtest of barrett test

---
+++ src/main/java/ml/psychology/api/domain/barrett/template/SequentialReasoningTemplate.java
@@ -0,0 +1,24 @@
+package ml.psychology.api.domain.barrett.template;
+
+import lombok.Data;
+import ml.psychology.api.domain.barrett.converter.StringArrayToString;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Data
+@Entity
+public class SequentialReasoningTemplate {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotNull
+    private String imageUrl;
+
+    @NotNull
+    @Convert(converter = StringArrayToString.class)
+    private List<String> optionImageUrls;
+}
