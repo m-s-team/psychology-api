45bb64cc3db24fc672a8b62cc7013ccab4802bec
Hasan Karimi <hasankarimi.dev@gmail.com>
2022-11-15 13:36:03 +0330

    feat: add required time to barrett subtest DTO

--- src/main/java/ml/psychology/api/service/barrett/dto/SubtestDTO.java
+++ src/main/java/ml/psychology/api/service/barrett/dto/SubtestDTO.java
@@ -1,14 +1,17 @@
 package ml.psychology.api.service.barrett.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
 
 import java.time.Instant;
 
+@Data
 @Schema(name = "Subtest")
-public record SubtestDTO(
+public class SubtestDTO {
         @Schema(description = "Creation time of this subtest", example = "2022-08-03T02:32:30.100603994Z")
-        Instant createdDate,
+        Instant createdDate;
+        @Schema(description = "Required times for this subtest in minutes", example = "8")
+        int requiredTime;
         @Schema(description = "Completed time of this subtest", example = "2022-08-03T02:45:12.10865477Z")
-        Instant completedDate
-) {
+        Instant completedDate;
 }
--- src/main/java/ml/psychology/api/service/barrett/mapper/BarrettTestMapper.java
+++ src/main/java/ml/psychology/api/service/barrett/mapper/BarrettTestMapper.java
@@ -1,11 +1,24 @@
 package ml.psychology.api.service.barrett.mapper;
 
+import ml.psychology.api.config.Constants;
 import ml.psychology.api.domain.barrett.BarrettTest;
 import ml.psychology.api.service.barrett.dto.BarrettTestDTO;
+import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface BarrettTestMapper {
 
     BarrettTestDTO entityToDto(BarrettTest entity);
+
+    @AfterMapping
+    default void entityToDto(@MappingTarget BarrettTestDTO barrettTestDTO) {
+        barrettTestDTO.visualReasoningSubtest().setRequiredTime(Constants.VISUAL_REASONING_REQUIRED_MINUTE);
+        barrettTestDTO.numericalReasoningSubtest().setRequiredTime(Constants.NUMERICAL_REASONING_REQUIRED_MINUTE);
+        barrettTestDTO.verbalAnalysisSubtest().setRequiredTime(Constants.VERBAL_ANALYSIS_REQUIRED_MINUTE);
+        barrettTestDTO.sequentialReasoningSubtest().setRequiredTime(Constants.SEQUENTIAL_REASONING_REQUIRED_MINUTE);
+        barrettTestDTO.spatialRecognitionSubtest().setRequiredTime(Constants.SPATIAL_RECOGNITION_REQUIRED_MINUTE);
+        barrettTestDTO.threeDSubtest().setRequiredTime(Constants.SPATIAL_RECOGNITION_REQUIRED_MINUTE);
+    }
 }
